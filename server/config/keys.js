c22f6f0541b46e2bd6dc5aa62e827a3cb51eff88
Spyros-Kontolatis <spiros.kodolatis@gmail.com>
2022-05-10 12:32:53 +0300

    fixed environment variables

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,3 +1,8 @@
+const dotenv = require("dotenv")
+dotenv.config()
+
+
+
 module.exports = {
-    Bands: 'mongodb+srv://bands-admin:R72rj4jfeU0otw84@cluster0.6e1g9.mongodb.net/bands?retryWrites=true&w=majority'
+    Bands: `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.6e1g9.mongodb.net/bands?retryWrites=true&w=majority`
 }
